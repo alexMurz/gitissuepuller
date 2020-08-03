1ef3a342ca45af7cd1c745e6e3c37c3833459f05
alexmurz <alex_murz@icloud.com>
2020-08-03 20:34:00 +0300

    SubscriptionsRepository.kt initial loading now disposes Disposable after loading automagically

--- domain/src/main/java/com/example/gitissuepull/domain/repo/SubscriptionsRepository.kt
+++ domain/src/main/java/com/example/gitissuepull/domain/repo/SubscriptionsRepository.kt
@@ -3,6 +3,8 @@ package com.example.gitissuepull.domain.repo
 import com.example.gitissuepull.domain.data.Repository
 import com.example.gitissuepull.domain.uses.UseCaseLoadSubscriptions
 import com.example.gitissuepull.domain.uses.UseCaseSaveSubscriptions
+import io.reactivex.SingleObserver
+import io.reactivex.disposables.Disposable
 
 class SubscriptionsRepository(
     private val useLoad: UseCaseLoadSubscriptions,
@@ -12,11 +14,19 @@ class SubscriptionsRepository(
     // Subs list
     val subs = ArrayList<Repository>()
     init {
-        useLoad.loadSubscriptions().subscribe(
-            { it -> subs.addAll(it) },
-            { err -> }
-        )
-        useSave.saveSubscriptions(subs)
+        useLoad.loadSubscriptions().subscribe(object : SingleObserver<List<Repository>> {
+            var d: Disposable? = null
+            override fun onSubscribe(d: Disposable) {
+                this.d = d
+            }
+            override fun onSuccess(t: List<Repository>) {
+                subs.addAll(t)
+                d?.dispose()
+            }
+            override fun onError(e: Throwable) {
+                e.printStackTrace()
+            }
+        })
     }
 
     override fun onListenerAdded(l: Callback) = l.subscriptionsUpdated(subs)
