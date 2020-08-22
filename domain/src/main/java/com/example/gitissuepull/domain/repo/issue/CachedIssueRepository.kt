7c2a941188452e36889cf5ac11ed4bdedd33bca7
alexmurz <alex_murz@icloud.com>
2020-08-22 12:08:50 +0300

    CachedIssueRepository, new cached repository for Issue list

--- domain/src/main/java/com/example/gitissuepull/domain/repo/issue/CachedIssueRepository.kt
+++ domain/src/main/java/com/example/gitissuepull/domain/repo/issue/CachedIssueRepository.kt
@@ -1,24 +1,41 @@
 package com.example.gitissuepull.domain.repo.issue
 
+import com.example.gitissuepull.domain.cache.TimedCache
+import com.example.gitissuepull.domain.cache.TimedCacheSerializer
 import com.example.gitissuepull.domain.data.Issue
 
 /// Extension of BasicIssueRepository, use HashMap to hold already loaded List<Issue> for User, Repo
 /// Yield cached data on `get` call, and update cache on `load` call
 class CachedIssueRepository(
-    issueSource: IssueRepository.UseGet
+    issueSource: IssueRepository.UseGet,
+    cacheLoad: TimedCacheSerializer.Deserializer<String, List<Issue>>,
+    cacheSave: TimedCacheSerializer.Serializer<String, List<Issue>>
 ): BasicIssueRepository(issueSource) {
 
-    private val cache = HashMap<String, List<Issue>>()
+    private val serializer = TimedCacheSerializer(cacheSave, cacheLoad)
+    private val cache = TimedCache<String, List<Issue>>(10000).also {
+        serializer.deserialize(it)
+    }
+
     private fun formatName(user: String, repo: String): String = user + repo
 
     override fun get(user: String, repo: String) {
-        val cache = cache[formatName(user, repo)] ?: return super.get(user, repo)
+        val cache = cache.get(formatName(user, repo)) ?: return super.get(user, repo)
         callback { onSuccess(cache) }
     }
 
     override fun loadComplete(user: String, repo: String, result: List<Issue>) {
-        cache[formatName(user, repo)] = result
+        cache.insert(formatName(user, repo), result)
+        // Queue cache serialization
+        serializer.serialize(cache)
         super.loadComplete(user, repo, result)
     }
 
+    override fun loadFailed(user: String, repo: String, error: Throwable) {
+        // Propagate error and cached data, if present
+        cache.rawGet(formatName(user, repo))?.let {
+            callback { onSuccess(it) }
+        }
+        super.loadFailed(user, repo, error)
+    }
 }
